166683f9292ccadd37e2b324ea10b3ffba30da40
shacharml <shacharml611@gmail.com>
2020-08-13 12:39:01 +0300

    set a new Sort by hour departure option 2

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,87 @@
-	import java.util.ArrayList;
-	import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-	public class Main {
+public class Main {
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 
-			ArrayList <travel> allTravels = new ArrayList<travel>();
-			int choose = 0 , i=0;
-			boolean fCountinue = true;
-			Scanner s = new Scanner(System.in);
-			
-			String startingState, lastState;
-			int hour, minute;
+		ArrayList<travel> allTravels = new ArrayList<travel>();
+		int choose = 0, i = 0;
+		boolean fCountinue = true;
+		Scanner s = new Scanner(System.in);
 
-			do {
+		String startingState, lastState;
+		int hour, minute;
 
-				System.out.println(" 1- Enter your travel deatails ");
-				System.out.println(" 2- Sort by departure time ");
-				System.out.println(" 9- Exit ");
+		do {
 
-				choose = s.nextInt();
-				s.nextLine();
-			
+			System.out.println(" 1- Enter your travel deatails ");
+			System.out.println(" 2- Sort by departure time ");
+			System.out.println(" 9- Exit ");
+
+			choose = s.nextInt();
+			s.nextLine();
 
 			switch (choose) {
 			case 1: {
 				System.out.println("Enter your starting state : ");
 				startingState = s.nextLine();
-				
+
 				System.out.println("Enter your last state : ");
 				lastState = s.nextLine();
-				
-				
-	            System.out.println("Enter hour : ");
+
+				System.out.println("Enter hour : ");
 				hour = s.nextInt();
-				
+
 				System.out.println("Enter minute : ");
 				minute = s.nextInt();
-	            
-				
-				while (hour >24 ||hour <=0 ) {
+
+				while (hour > 24 || hour <= 0) {
 					System.out.println("Enter hour again : ");
 					hour = s.nextInt();
 				}
-				while (minute > 59 ||minute < 0 ) {
+				while (minute > 59 || minute < 0) {
 					System.out.println("Enter minute again : ");
 					minute = s.nextInt();
 				}
-				
+
 				travel travel = new travel(startingState, lastState, hour, minute);
 				allTravels.add(travel);
+
 				
-				System.out.println(allTravels.toString());
-				
-				
+				//System.out.println(allTravels.toString());
+
 				break;
 			}
 			case 2: {
+				// sort by hour
+				
+				for (int z = 0; z < allTravels.size()-1; z++) {
+					
+					for (int j = 0; j < allTravels.size() - 1; j++) {
+						
+						if (allTravels.get(j).getHour() > allTravels.get(j + 1).getHour()) {
+							
+							travel temp = allTravels.get(j);
+							allTravels.set(j, allTravels.get(j + 1));
+							allTravels.set(j + 1, temp);
+						}
+						
+						if (allTravels.get(j).getHour() == allTravels.get(j + 1).getHour()) {
+							if (allTravels.get(j).getMinute() > allTravels.get(j + 1).getMinute()) {
+								travel temp = allTravels.get(j);
+								allTravels.set(j, allTravels.get(j + 1));
+								allTravels.set(j + 1, temp);
+							}
+						}
 
+					}
+
+				}
+
+				
+				System.out.println(allTravels.toString());
+				
 				break;
 			}
 			case 9: {
@@ -66,13 +90,9 @@
 			}
 			}
 
-			} while (fCountinue);
-			
-			s.close();
-		}
+		} while (fCountinue);
 
-
+		s.close();
 	}
 
-
-
+}
--- src/travel.java
+++ src/travel.java
@@ -4,26 +4,35 @@ public class travel {
 	private String lastgState;
 	private int hour;
 	private int minute;
-	
-	
-	public travel(String startingState , String lastState , int hour , int minute) {
-	
+
+	public travel(String startingState, String lastState, int hour, int minute) {
+
 		this.lastgState = lastState;
 		this.startingState = startingState;
 		this.hour = hour;
 		this.minute = minute;
-		
-		
-		
-		
-	}
 
+	}
 
 	@Override
 	public String toString() {
-		return "travel [startingState=" + startingState + ", lastgState="
-				+ lastgState + ", hour=" + hour + ", minute=" + minute + "]";
+		return "travel [Starting State=" + startingState + ", Lastg State=" + lastgState + ", Departure time = " + hour +":" + minute + "] \n";
 	}
-	
+
+	public int getHour() {
+		return hour;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	public int getMinute() {
+		return minute;
+	}
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
 
 }
