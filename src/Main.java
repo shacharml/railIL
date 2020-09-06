bd53fa26b559ed2a9c584b5e4d6104ec3dd3b06b
shacharml <shacharml611@gmail.com>
2020-09-06 19:04:21 +0300

    fixing bug and adding to main;

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		ArrayList<travel> allTravels = new ArrayList<travel>();
-		int choose = 0;
+		int choose = 0, i = 0;
 		boolean fCountinue = true;
 		Scanner s = new Scanner(System.in);
 
-		String startingState, lastState;
-		int hour, minute;
+		String startingState, lastState, stopovers;
+		int hour, minute, h1,m1;
 
 		do {
 
@@ -44,29 +44,54 @@ public class Main {
 					System.out.println("Enter minute again : ");
 					minute = s.nextInt();
 				}
+				
+				boolean b = true;
+				ArrayList<stopovers> allstStopovers = new ArrayList<stopovers>();
+				while(b) {
+				System.out.println("Enter  your stopovers :");
+				stopovers = s.nextLine();
+				System.out.println("Enter hour : ");
+				h1 = s.nextInt();
 
-				travel travel = new travel(startingState, lastState, hour, minute);
+				System.out.println("Enter minute : ");
+				m1 = s.nextInt();
+
+				while (h1 > 24 || h1 <= 0) {
+					System.out.println("Enter hour again : ");
+					h1 = s.nextInt();
+				}
+				while (m1 > 59 || m1 < 0) {
+					System.out.println("Enter minute again : ");
+					m1 = s.nextInt();
+				}
+				allstStopovers.add(new stopovers(stopovers, h1, m1));
+				System.out.println("you want another stop over? T/F ");
+				if(s.nextLine().equals("F")) {
+					b=false;
+				}
+				}
+
+				travel travel = new travel(startingState, lastState, hour, minute,allstStopovers);
 				allTravels.add(travel);
 
-				
-				//System.out.println(allTravels.toString());
+				// System.out.println(allTravels.toString());
 
 				break;
 			}
 			case 2: {
 				// sort by hour
-				
-				for (int z = 0; z < allTravels.size()-1; z++) {
-					
+
+				for (int z = 0; z < allTravels.size() - 1; z++) {
+
 					for (int j = 0; j < allTravels.size() - 1; j++) {
-						
+
 						if (allTravels.get(j).getHour() > allTravels.get(j + 1).getHour()) {
-							
+
 							travel temp = allTravels.get(j);
 							allTravels.set(j, allTravels.get(j + 1));
 							allTravels.set(j + 1, temp);
 						}
-						
+
 						if (allTravels.get(j).getHour() == allTravels.get(j + 1).getHour()) {
 							if (allTravels.get(j).getMinute() > allTravels.get(j + 1).getMinute()) {
 								travel temp = allTravels.get(j);
@@ -79,8 +104,11 @@ public class Main {
 
 				}
 
-				
 				System.out.println(allTravels.toString());
+
+				break;
+			}
+			case 3: {
 				
 				break;
 			}
--- src/travel.java
+++ src/travel.java
@@ -1,21 +1,25 @@
+import java.util.ArrayList;
 
 public class travel {
 	private String startingState;
 	private String lastgState;
 	private int hour;
 	private int minute;
-
-	public travel(String startingState, String lastState, int hour, int minute) {
+	private ArrayList<stopovers> allStopovers; 
+		
+	
+	public travel(String startingState, String lastState, int hour, int minute,ArrayList<stopovers> allStopovers ) {
 		this.lastgState = lastState;
 		this.startingState = startingState;
 		this.hour = hour;
 		this.minute = minute;
+		this.allStopovers=new ArrayList<stopovers>();
 	}
 
 	@Override
 	public String toString() {
 		return "travel [Starting State=" + startingState + ", Lastg State=" + lastgState + ", Departure time = "
-				+ show2() + "] \n";
+				+ show2() + "the stops overes between this stations: "+allStopovers.toString()+ "] \n";
 	}
 
 	public int getHour() {
