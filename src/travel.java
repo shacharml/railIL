e45cedbca0084bfb3e54235ef48a3a13622fb58c
shacharml <68264925+shacharml@users.noreply.github.com>
2020-09-14 18:35:37 +0300

    Update travel.java

--- src/travel.java
+++ src/travel.java
@@ -1,11 +1,14 @@
+
+import java.io.PrintWriter;
 import java.util.ArrayList;
+
 
 public class travel {
 	private String startingState;
 	private String lastgState;
 	private int hour;
 	private int minute;
-	private ArrayList<stopovers> allStopovers; 
+	private ArrayList<stopovers> allStopovers=new ArrayList<stopovers>(); 
 		
 	
 	public travel(String startingState, String lastState, int hour, int minute,ArrayList<stopovers> allStopovers ) {
@@ -13,7 +16,15 @@ public class travel {
 		this.startingState = startingState;
 		this.hour = hour;
 		this.minute = minute;
-		this.allStopovers=new ArrayList<stopovers>();
+		this.allStopovers= allStopovers;
+	}
+
+	public String getLastgState() {
+		return lastgState;
+	}
+
+	public void setLastgState(String lastgState) {
+		this.lastgState = lastgState;
 	}
 
 	@Override
@@ -28,6 +39,22 @@ public class travel {
 
 	public void setHour(int hour) {
 		this.hour = hour;
+	}
+
+	public String getStartingState() {
+		return startingState;
+	}
+
+	public ArrayList<stopovers> getAllStopovers() {
+		return allStopovers;
+	}
+
+	public void setAllStopovers(ArrayList<stopovers> allStopovers) {
+		this.allStopovers = allStopovers;
+	}
+
+	public void setStartingState(String startingState) {
+		this.startingState = startingState;
 	}
 
 	public int getMinute() {
@@ -69,5 +96,22 @@ public class travel {
 		return str;
 
 	}
+
+	public void saveTravel(PrintWriter pw) {
+		
+		pw.println(startingState);
+		pw.println(lastgState);
+		pw.println(hour);
+		pw.println(minute);
+		//how much stopovers are in this travel
+		pw.println(allStopovers.size());
+		
+		for (int i = 0; i < allStopovers.size(); i++) {
+			
+			allStopovers.get(i).saveStepover(pw);	
+		}
+		
+	}
+	
 
 }
