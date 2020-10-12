e2e99994a486053623a8908edb914557189cb60d
shacharml <68264925+shacharml@users.noreply.github.com>
2020-10-12 19:45:23 +0300

    Python file of the connection to the web and Java - sprint3
    This is the Python file 
    >>needs to open it on a new PyCharm>new flask project > add this file.
    *need to chang in this file the correct path of your computer : where the RaulIL exist/save
    and then it is needs to work :)

---
+++ railIL_web.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import subprocess
+
+from flask import Flask, request
+
+app = Flask("RailIl")
+
+#/rail?outfomate=html&startS=Binyamina&lastS=Modiin&hour=10&minutes=12
+#that waht we need to writh to this in after the URL for working
+
+@app.route("/rail")
+def rail_schedule():
+    if 'outformat' in request.args:
+        outformat = request.args.get('outformat')
+    else:
+        outformat = "html"
+
+    if 'startS' in request.args :
+        startS = request.args.get('startS')
+    else:
+        startS ='null'
+
+    if 'lastS' in request.args:
+        lastS = request.args.get('lastS')
+    else:
+        lastS = 'null'
+
+    if 'hour' in request.args:
+        hour = request.args.get('hour')
+    else:
+        hour = 'null'
+
+    if 'minutes' in request.args:
+        minutes = request.args.get('minutes')
+    else:
+        minutes = 'null'
+
+
+    return subprocess.check_output(["java", "-classpath", "/home/shachar/git/railIL/bin", "main_web/main_web", outformat
+                                   ,startS , lastS , hour, minutes])
+
+
+
